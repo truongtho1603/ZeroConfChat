debf2f41a76eece219250d4457f7134e1f8327c2
Tho Do <tho.do@medtronic.com>
2022-09-26 17:29:33 +0100

    Initial commit

---
+++ ZeroConfChat/ZeroConfChat/ZeroConfChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZeroConfChatApp.swift
+//  ZeroConfChat
+//
+//  Created by Do, Tho on 26/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct ZeroConfChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
